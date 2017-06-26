a0d6ec0f59d36a9102defe5960746fe79944ef94
rostislav.dudka <rostislav.dudka@gmail.com>
2017-06-26 13:20:47 +0300

    -refactor dashboard controller;

--- src/main/java/ua/dudka/account/web/DashboardController.java
+++ src/main/java/ua/dudka/account/web/DashboardController.java
@@ -2,9 +2,7 @@ package ua.dudka.account.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ua.dudka.account.application.CurrentAccountReader;
 
 import static ua.dudka.account.web.DashboardController.Links.DASHBOARD_PAGE_URL;
 
@@ -13,19 +11,14 @@ import static ua.dudka.account.web.DashboardController.Links.DASHBOARD_PAGE_URL;
  */
 @Controller
 @RequiredArgsConstructor
-public class DashboardController {
-
-    private final CurrentAccountReader currentAccountReader;
+public class DashboardController extends AccountController {
 
     @GetMapping(DASHBOARD_PAGE_URL)
-    public String getPage(Model model) {
-        model.addAttribute("account", currentAccountReader.read());
+    public String getPage() {
         return "account/dashboard";
     }
 
-
     public static class Links {
         public static final String DASHBOARD_PAGE_URL = "/dashboard";
-
     }
 }
